63b7cb93fdac3e808da45431fce4dd9419839df2
Muhammad Sameer <muhammadsameer@Sameers-Mac.local>
2024-07-02 17:58:45 +0500

    added authorization using sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: 'api',
     )
     ->withRouting(
         api: __DIR__.'/../routes/api_v1.php',
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Api\v1\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
